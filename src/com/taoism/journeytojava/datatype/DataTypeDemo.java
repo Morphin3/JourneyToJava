361f2d5ee3dba525aae1e32e4032d4477e44fd18
Morphin3 <morphin3@aliyun.com>
2014-11-05 13:57:06 +0800

    feat(项目IDE):从Eclipse迁移到Intellij IDEA

--- src/com/taoism/journeytojava/datatype/DataTypeDemo.java
+++ src/com/taoism/journeytojava/datatype/DataTypeDemo.java
@@ -1,18 +1,25 @@
 package com.taoism.journeytojava.datatype;
 
+import com.sun.source.tree.IfTree;
+
+
 public class DataTypeDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		isNaNTest();
 	}
-	
-	private static void  isNaNTest(){
-		System.out.println(3==Double.NaN);
-		
-		double x=0;
-		double y=0;
-		System.out.println(Double.isNaN(x/y));
-	}
 
+	private static void isNaNTest() {
+		System.out.println(3 == Double.NaN); // x==Double.NaN is Never True
+
+		double x = 0;
+		double y = 0;
+		System.out.println(Double.isNaN(x / y));
+		
+		System.out.println("\u005B\u005D");
+	}
+    
+	
+	
 }
