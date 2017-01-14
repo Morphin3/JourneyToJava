4b5f07d2bfad83f573bf0ecdd2945876928b7225
Morphin3 <morphin333@gmail.com>
2017-01-14 16:10:08 +0800

    static block test

---
+++ src/com/taoism/journeytojava/littletest/NoMainClass.java
@@ -0,0 +1,14 @@
+package com.taoism.journeytojava.littletest;
+
+/**
+ * Date: 2016-08-05
+ * Time: 09:57
+ * Author: cf
+ * -----------------------------
+ */
+public class NoMainClass {
+    static {
+        System.out.println("Hello World!");
+        System.exit(0);
+    }
+}
