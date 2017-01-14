7731d8bcc9fe4215bf3f0f836dfd1adb65b3b9da
Morphin3 <morphin333@gmail.com>
2017-01-14 16:13:17 +0800

    overall

--- src/com/taoism/journeytojava/Overall.java
+++ src/com/taoism/journeytojava/Overall.java
@@ -10,37 +10,74 @@ public class Overall {
         int fourthBitFromRight = (n & 8) / 8;
         System.out.println(fourthBitFromRight);
 
-        System.out.println(Math.pow(2.1,2.3));
+        System.out.println(Math.pow(2.1, 2.3));
         System.out.println(Math.sin(30));
-        System.out.println(Math.sin(3.14/6));
+        System.out.println(Math.sin(3.14 / 6));
         System.out.println(Math.cos(30));
 
-        System.out.println(StrictMath.sin(3.14/6));
+        System.out.println(StrictMath.sin(3.14 / 6));
 
 
-
-        int i=1;
-        int j=1;
-        if(i++ <0&j++>0){
+        int i = 1;
+        int j = 1;
+        if (i++ < 0 & j++ > 0) {
 
         }
         System.out.println(j);
-        i=0;
-        j=1;
-        if(i++ <0&&j++>0){
+        i = 0;
+        j = 1;
+        if (i++ < 0 && j++ > 0) {
 
         }
         System.out.println(j);
 
         int n1 = 10;
         int n2 = 7;
-        System.out.println(n1%n2==(n1&(n2-1)));
+        System.out.println(n1 % n2 == (n1 & (n2 - 1)));
 
-        System.out.println(n1%n2);
-        System.out.println(n1&(n2-1));
+        System.out.println(n1 % n2);
+        System.out.println(n1 & (n2 - 1));
+
+        String s = "hello";
+        char[] chars = {'a', 'b', 'c', 'd'};
 
 
+        exchange(s, chars);
+
+        System.out.println(s);
+        System.out.println(chars);
+
+        OverallTestModel a = new OverallTestModel("a", 0, false);
+        OverallTestModel b = new OverallTestModel("b", 1, true);
+
+        exchange(a, b);
+
+        System.out.println(a.getS());
+
+        System.out.println(17%-10);
+
+        getEnclosingClassTest();
 
     }
+
+    public static OverallTestModel bb = new OverallTestModel("b", 1, true);
+
+    public static void exchange(String str, char[] chars) {
+        str = "boy";
+        chars = new char[]{'a', 'a', 'c', 'd'};
+//        chars[1] = 'a';
+    }
+
+    public static void exchange(OverallTestModel a, OverallTestModel b) {
+//        a = new OverallTestModel("c", 2, true);
+
+        a.setS("d");
+//        a = bb;
+    }
+
+    public static void getEnclosingClassTest(){
+        System.out.println(new Object(){}.getClass().getEnclosingClass());
+    }
+
 
 }
---
+++ src/com/taoism/journeytojava/OverallTestModel.java
@@ -0,0 +1,43 @@
+package com.taoism.journeytojava;
+
+/**
+ * Date: 2016-07-15
+ * Time: 17:48
+ * Author: cf
+ * -----------------------------
+ */
+public class OverallTestModel {
+    String s;
+    int i;
+    boolean b;
+
+    public OverallTestModel(String s, int i, boolean b) {
+        this.s = s;
+        this.i = i;
+        this.b = b;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public void setS(String s) {
+        this.s = s;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public boolean isB() {
+        return b;
+    }
+
+    public void setB(boolean b) {
+        this.b = b;
+    }
+}
